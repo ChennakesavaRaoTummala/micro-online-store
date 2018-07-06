3eeba83a336eb43790674c4aef03c50e407e0ff4
Chennakesava Rao <ckrao2@apple.com>
2018-07-06 15:14:25 +0530

    first commit- generated application

---
+++ src/main/java/com/infy/store/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.infy.store.repository;
+
+import com.infy.store.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/infy/store/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.infy.store.web.websocket.dto;
